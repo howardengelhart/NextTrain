4adde57a1f95add38c0acd953049a86fbd229f26
Howard Engelhart <howard.engelhart@outlook.com>
2016-09-29 20:39:21 -0400

    adding support for using local otp,dynamdob servers

---
+++ scripts/createTables.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var aws = require("aws-sdk");
+
+aws.config.update({
+      region:   'us-east-1',
+      endpoint: 'http://localhost:8000'
+});
+
+const dynamodb = new aws.DynamoDB();
+
+let params = {
+    TableName : "Movies",
+    KeySchema: [       
+        { AttributeName: "year", KeyType: "HASH"},  //Partition key
+        { AttributeName: "title", KeyType: "RANGE" }  //Sort key
+    ],
+    AttributeDefinitions: [       
+        { AttributeName: "year", AttributeType: "N" },
+        { AttributeName: "title", AttributeType: "S" }
+    ],
+    ProvisionedThroughput: {       
+        ReadCapacityUnits: 10, 
+        WriteCapacityUnits: 10
+    }
+};
+
+dynamodb.createTable(params, (err, data) => {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
---
+++ scripts/otpq.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl "http://localhost:8080/otp/routers/default/plan?fromPlace=1%3A124&toPlace=1%3A105&time=11%3A45am&date=09-30-2016&mode=TRANSIT%2CWALK&maxWalkDistance=804.672&arriveBy=true&wheelchair=false&locale=en"
