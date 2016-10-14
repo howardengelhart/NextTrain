2d9cfda381bc4782c935f1b66b92e0f49995e6e8
Howard Engelhart <howard.engelhart@outlook.com>
2016-10-14 16:24:25 -0400

    updated some scripts

--- scripts/otpq.sh
+++ scripts/otpq.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #curl "http://localhost:8080/otp/routers/default/plan?fromPlace=1%3A124&toPlace=1%3A105&time=11%3A45am&date=09-30-2016&mode=TRANSIT%2CWALK&maxWalkDistance=804.672&arriveBy=true&wheelchair=false&locale=en"
 
-curl "http://54.175.14.6:8080/otp/routers/default/plan?fromPlace=1%3A05&toPlace=1%3A124&mode=TRANSIT%2CWALK&maxWalkDistance=804.672&wheelchair=false&locale=en&numItineraries=5&showIntermediateStops=true&arriveBy=true&date=10-10-2016&time="
+#curl "http://54.175.14.6:8080/otp/routers/default/plan?fromPlace=1%3A05&toPlace=1%3A124&mode=TRANSIT%2CWALK&maxWalkDistance=804.672&wheelchair=false&locale=en&numItineraries=5&showIntermediateStops=true&arriveBy=true&date=10-15-2016&time="
 
 #curl "http://54.175.14.6:8080/otp/routers/default/index/trips/1%3A2767"
 
 # Find stops
-#curl "http://54.175.14.6:8080/otp/routers/default/index/stops?lat=40.353988319427&lon=-74.640870012458&radius=5000"
+curl "http://54.175.14.6:8080/otp/routers/default/index/stops?lat=40.353988319427&lon=-74.640870012458&radius=5000"
