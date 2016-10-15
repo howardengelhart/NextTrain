8b277b08837169fa54ef7e1b1562da8d8612e0b7
Howard Engelhart <howard.engelhart@outlook.com>
2016-10-15 12:24:29 -0400

    updates to otqp script

--- scripts/otpq.sh
+++ scripts/otpq.sh
@@ -6,4 +6,8 @@
 #curl "http://54.175.14.6:8080/otp/routers/default/index/trips/1%3A2767"
 
 # Find stops
-curl "http://54.175.14.6:8080/otp/routers/default/index/stops?lat=40.353988319427&lon=-74.640870012458&radius=5000"
+#curl "http://52.2.216.51:8080/otp/routers/default/index/stops?lat=40.353988319427&lon=-74.640870012458&radius=5000"
+
+#curl "http://52.2.216.51:8080/otp/routers/bart/plan?fromPlace=1%3A124&toPlace=1%3A105&mode=TRANSIT%2CWALK&maxWalkDistance=804.672&&wheelchair=false&locale=en"
+
+curl "http://otp.mechinate.com:8080/otp/routers/bart/plan?fromPlace=1%3AASHB&toPlace=1%3APOWL&showIntermediateStops=true&ignoreRealtimeUpdates=false"
