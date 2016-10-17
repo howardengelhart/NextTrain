e9e678dfbc79e581ff8d5c65bca69b58f029070c
Howard Engelhart <howard.engelhart@outlook.com>
2016-10-16 22:16:25 -0400

    added match for testing fuzzy matches

---
+++ scripts/match.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const OTPlanner = require('../src/OTPlanner');
+const fuzzy = require('fuzzy');
+
+let routerId = process.argv[2];
+let search = process.argv[3];
+let otp = new OTPlanner({ hostname : 'otp.mechinate.com:8080', routerId : routerId });
+
+console.log(`Look for "${search}" using router ${routerId}`);
+otp.findStops()
+.then(results => {
+    let matches = fuzzy.filter(search,results,{ extract: (s=> s.name)}).map(m => m.original);
+    if (!matches.length) {
+        let c = search.replace(/\W+/g,' ').split(' ');
+        console.log('Search: ', c);
+        matches = fuzzy.filter(c[0],results,{ extract: (s=> s.name)}).map(m => m.original);
+    }
+
+    console.log(matches);
+})
+.catch(err => {
+    console.log(err);
+});
+
+
+
